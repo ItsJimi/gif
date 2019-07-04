ff98e3778d5322ebb588bddd5f5349d73a92e3c1
Jimi M <jimi.maiquez@gmail.com>
2019-07-04 20:45:23 +0200

    convert .mov in folder to gif

--- cmd/convert.go
+++ cmd/convert.go
@@ -1,19 +1,56 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/ItsJimi/gif-cli/pkg/convert"
 	"github.com/spf13/cobra"
 )
 
 func init() {
+	convertCmd.Flags().StringVarP(&crop, "crop", "c", "", "Crop videos before conversion")
 	rootCmd.AddCommand(convertCmd)
 }
 
 var convertCmd = &cobra.Command{
-	Use:   "convert",
-	Short: "Convert videos to animated gif",
-	Long:  "Convert videos to animated gif.",
+	Use:     "convert",
+	Short:   "Convert videos to animated gif",
+	Long:    "Convert videos to animated gif.",
+	Example: `  gif convert my-video.mp4 my-gif.gif`,
 	Run: func(cmd *cobra.Command, args []string) {
-		convert.Convert()
+		if len(args) == 0 {
+			fmt.Println("Invalid arguments, type `gif convert -h` to see arguments available.")
+			return
+		}
+
+		inputPath, err := os.Stat(args[0])
+		if err != nil {
+			fmt.Println(args[0] + ": no such file or directory")
+			return
+		}
+
+		if inputPath.Mode().IsDir() == true {
+			if len(args) != 2 {
+				fmt.Println("Invalid arguments, type `gif convert -h` to see arguments available.")
+				return
+			}
+
+			outputPath, err := os.Stat(args[1])
+			if err != nil || outputPath.Mode().IsDir() == false {
+				fmt.Println(args[1] + ": no such directory")
+				return
+			}
+
+			convert.FromFolder(inputPath.Name(), outputPath.Name())
+			return
+		}
+
+		crop := cmd.Flag("crop")
+		if crop.Changed == false {
+			// convert.FromFile()
+		}
 	},
 }
+
+var crop string
--- pkg/convert/convert.go
+++ pkg/convert/convert.go
@@ -1,8 +1,37 @@
 package convert
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
 
-// Convert video to gif
-func Convert() {
-	fmt.Println("test")
+// FromFolder convert videos to gif from folder
+func FromFolder(inputPath string, outputPath string) {
+	var files []os.FileInfo
+
+	err := filepath.Walk(inputPath, func(path string, info os.FileInfo, err error) error {
+		if strings.ToLower(filepath.Ext(path)) != ".mov" {
+			return nil
+		}
+		files = append(files, info)
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(inputPath + " -> " + outputPath)
+	for _, file := range files {
+		fmt.Println("Converting " + file.Name() + " to " + file.Name() + ".gif")
+		ffmpeg := exec.Command("ffmpeg", "-i", inputPath+"/"+file.Name(), "-vf", "fps=30,scale=1920:-1:flags=lanczos,palettegen", "-y", outputPath+"/palette.png")
+		if err := ffmpeg.Run(); err != nil {
+			fmt.Println(err)
+		}
+		ffmpeg = exec.Command("ffmpeg", "-i", inputPath+"/"+file.Name(), "-i", outputPath+"/palette.png", "-filter_complex", "fps=30,scale=500:-1:flags=lanczos[x];[x][1:v]paletteuse", "-y", outputPath+"/"+file.Name()+".gif")
+		if err = ffmpeg.Run(); err != nil {
+			fmt.Println(err)
+		}
+	}
 }
